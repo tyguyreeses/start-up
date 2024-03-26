9789cae687caea3243e146aa37df3688b27edca2
Tyler <tyguyreeses@gmail.com>
2024-03-25 21:34:28 -0600

    adding code for chat system

--- public/compare.js
+++ public/compare.js
@@ -1,12 +1,70 @@
-// let sortDirection = 1;
-// let entriesData = [];
+// chat functionality
+// Adjust the webSocket protocol to what is being used for HTTP
+const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+const socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
 
+// Display that we have opened the webSocket
+socket.onopen = (event) => {
+  appendMsg('system', 'websocket', 'connected');
+};
+
+// Display messages we receive from our friends
+socket.onmessage = async (event) => {
+  const text = await event.data.text();
+  const chat = JSON.parse(text);
+  appendMsg('friend', chat.name, chat.msg);
+};
+
+// If the webSocket is closed then disable the interface
+socket.onclose = (event) => {
+  appendMsg('system', 'websocket', 'disconnected');
+  document.querySelector('#chat-controls').disabled = true;
+};
+
+// Send a message over the webSocket
+function sendMessage() {
+  const msgEl = document.querySelector('#new-msg');
+  const msg = msgEl.value;
+  if (!!msg) {
+    appendMsg('me', 'me', msg);
+    const name = document.querySelector('#username_display').value;
+    socket.send(`{"name":"${name}", "msg":"${msg}"}`);
+    msgEl.value = '';
+  }
+}
+
+// Create one long list of messages
+function appendMsg(cls, from, msg) {
+  const chatText = document.querySelector('#chat-text');
+  const chatEl = document.createElement('div');
+  chatEl.innerHTML = `<span class="${cls}">${from}</span>: ${msg}</div>`;
+  chatText.prepend(chatEl);
+}
+
+// Send message on enter keystroke
+const input = document.querySelector('#new-msg');
+input.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    sendMessage();
+  }
+});
+
+// Disable chat if no name provided
+const chatControls = document.querySelector('#chat-controls');
+const myName = document.querySelector('#username_display');
+myName.addEventListener('keyup', (e) => {
+  chatControls.disabled = myName.value === '';
+});
+
+
+
+// ============================================================ //
+
+
+
+
+// entries functionality
 async function loadEntries() {
-    // const previousEntriesJSON = localStorage.getItem('previousEntries');
-    // if (previousEntriesJSON) {
-    //   entriesData = JSON.parse(previousEntriesJSON);
-    // }
-    // displayEntries(entriesData);
 
     let entries = [];
     try {
