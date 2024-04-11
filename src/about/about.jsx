6212a8930604cb988e7b8834f51e0c6497815285
Tyler <tyguyreeses@gmail.com>
2024-04-10 22:48:15 -0600

    adding about page skeleton

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 export function About() {
     return (
       <main>
-        <div>about displayed here</div>
+        <p>
+          This is where I will explain the purpose of this website in intellectual language!
+        </p>
+        <div id="quote">
+          <div id="quoteContent">Words are cheap. Show me the code.</div>
+          <div id="quoteAuthor">- Linus Torvalds</div>
+        </div>
       </main>
     );
   }
