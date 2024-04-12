bbc8190cad8dbaa356136e60fd8e14a8a3582e2b
Tyler <tyguyreeses@gmail.com>
2024-04-12 11:47:41 -0600

    debugging testForm() and calculate()

--- src/jobEntry/jobEntry.jsx
+++ src/jobEntry/jobEntry.jsx
@@ -6,7 +6,7 @@ class Entry extends Component {
 
     // Initialize state
     this.state = {
-        username: '',
+        username: localStorage.getItem("username"),
         name: '',
         salary: 0.0,
         bonus: 0.0,
@@ -17,7 +17,9 @@ class Entry extends Component {
         stockAmt: 0,
         stockPrice: 0.0,
         errorMessage: '',
-        showSubmitButton: false
+        showSubmitButton: false,
+        correctForm: false,
+        calculated: false
     };
   }
 
@@ -28,53 +30,63 @@ class Entry extends Component {
 
   testForm = () => {
     // Implement testForm function
+    console.log("Running testForm...");
     const { name, salary, bonus, stockAmt } = this.state;
-
+    console.log(`name: ${name}, salary: ${salary}, bonus: ${bonus}, stockAmt: ${stockAmt}`);
     if (!name || !salary) {
       alert('Please fill in all required fields.');
-      return false;
+      this.setState({ correctForm: false });
+      return;
     }
 
     if (isNaN(parseFloat(salary)) || isNaN(parseFloat(bonus)) || isNaN(parseFloat(stockAmt))) {
        alert("Please enter a valid input.\n(Don't include '$' or commas in numeric fields)");
-      return false;
+       this.setState({ correctForm: false });
+       return;
     }
+    console.log("Setting correctForm to true");
+    this.setState({ correctForm: true });
 
   }
 
   calculateSalary = () => {
     // Implement calculateSalary function
-    if (this.testForm) {
+    console.log("Running calculateSalary...");
+    this.testForm();
+    console.log("this.state.correctForm: ", this.state.correctForm);
+    if (this.state.correctForm) {
+      console.log("Passed testForm");
       // calculate stock grants
       if(this.stockTicker) {
+        console.log("Passed stockTicker");
         this.getStockPrice();
       }
       // calculate yearly takehome
-      const calculatedYearly = parseFloat(this.state.salary) + parseFloat(this.state.bonus);
-      let calculatedPeriod;
-      if (this.state.period === ' Monthly') {
-          calculatedPeriod = parseFloat(calculatedYearly / 12).toFixed(2);
-      } else if (this.state.period === ' Bi-Weekly') {
-          calculatedPeriod = parseFloat(calculatedYearly / 26).toFixed(2);
-      } else if (this.state.period === ' Weekly') {
-          calculatedPeriod = parseFloat(calculatedYearly / 52).toFixed(2);
+      const calcYearly = parseFloat(this.state.salary) + parseFloat(this.state.bonus);
+      let calcPeriod;
+      if (this.state.period === 'Monthly') {
+          calcPeriod = parseFloat(calcYearly / 12).toFixed(2);
+      } else if (this.state.period === 'Bi-Weekly') {
+          calcPeriod = parseFloat(calcYearly / 26).toFixed(2);
+      } else if (this.state.period === 'Weekly') {
+          calcPeriod = parseFloat(calcYearly / 52).toFixed(2);
       }
+      console.log("updating the states variables...");
       this.setState({
-        calculatedYearly: calculatedYearly,
-        calculatedPeriod: calculatedPeriod,
-        showSubmitButton: true
+        calculatedYearly: calcYearly,
+        calculatedPeriod: calcPeriod,
+        showSubmitButton: true,
+        calculated: true
       })
+      console.log(`showSubmitButton: ${this.state.showSubmitButton}, calculated: ${this.state.calculated}`);
       // display calculated salaries
-      this.displayCalculatedSalaries();
-      return true;
-    } else {
-        return false;
+      // this.displayCalculatedSalaries();
     }
   }
 
-  displayCalculatedSalaries = () => {
-    // Implement displayCalculatedSalaries function
-  }
+  // displayCalculatedSalaries = () => {
+  //   // Implement displayCalculatedSalaries function
+  // }
 
   getStockPrice = async () => {
     // Implement getStockPrice function
@@ -95,9 +107,15 @@ class Entry extends Component {
 
   resetStyle = () => {
     // Implement resetStyle function
+    this.setState({
+      showSubmitButton: false,
+      correctForm: false,
+      calculated: false
+    })
   }
 
   render() {
+    console.log("this.state.calculated is: ", this.state.calculated)
     return (
       <main>
         <form className="form" action="" onSubmit={this.handleSubmit}>
@@ -105,28 +123,28 @@ class Entry extends Component {
             <section className="form_section" id="company_info">
               <h1>Company Info</h1>
               <div className="info_label">
-                <label hmtlFor="title">Company Name: </label>
-                <input className="info_input" type="text" id="title" />
+                <label htmlFor="title">Company Name: </label>
+                <input className="info_input" type="text" id="title" placeholder="enter name" name="name" onChange={this.handleInputChange}/>
               </div>
               <div className="info_label">
-                <label hmtlFor="salary">Yearly Salary: </label>
-                <input className="info_input" type="text" id="salary" />
+                <label html="salary">Yearly Salary: </label>
+                <input className="info_input" type="text" id="salary" placeholder="enter salary" name="salary" onChange={this.handleInputChange}/>
               </div>
               <div className="info_label">
-                <label hmtlFor="bonus">Annual Bonus: </label>
-                <input className="info_input" type="text" id="bonus" />
+                <label html="bonus">Annual Bonus: </label>
+                <input className="info_input" type="text" id="bonus" placeholder="0" name="bonus" onChange={this.handleInputChange}/>
               </div>
               <div className="info_label">
-                <label hmtlFor="pay_period">Pay Period:  </label>
-                <select id="pay_period" name="pay_period">
-                  <option value=" Monthly">Monthly</option>
-                  <option value=" Bi-Weekly">Bi-Weekly</option>
-                  <option value=" Weekly">Weekly</option>
+                <label html="pay_period">Pay Period:  </label>
+                <select id="pay_period" name="period" onChange={this.handleInputChange} value={this.state.period}>
+                  <option value="Monthly">Monthly</option>
+                  <option value="Bi-Weekly">Bi-Weekly</option>
+                  <option value="Weekly">Weekly</option>
                 </select>
               </div>
             </section>
             <section className="form_section" id="stock_options">
-              <h1>Stock Options</h1>
+              <h1>Stock Options (optional)</h1>
               <table>
                 <thead>
                   <tr>
@@ -138,10 +156,10 @@ class Entry extends Component {
                 <tbody>
                   <tr>
                     <td>
-                      <input className="stock_input" type="text" id="stock_tag" />
+                      <input className="stock_input" type="text" id="stock_tag" placeholder="---"/>
                     </td>
                     <td>
-                      <input className="stock_input" type="text" id="stock_amt" />
+                      <input className="stock_input" type="text" id="stock_amt" placeholder="---"/>
                     </td>
                     <td className="stock_input" id="stock_price">---</td>
                   </tr>
@@ -149,15 +167,15 @@ class Entry extends Component {
               </table>
             </section>
           </section>
-          <div>
-            {this.calculateSalary() ? (
+          <section>
+            {this.state.calculated ? (
               <section id="salaryCalculation">
                 <div className="salaryInfo" id="yearlySalaryLabel">Yearly Takehome: ${this.state.calculatedYearly}</div>
                 <div className="salaryInfo" id="timeSalaryLabel">{this.state.period} Takehome: ${this.state.calculatedPeriod}</div>
                 <div className="salaryInfo" id="stockLabel">Stock Benefits: ${this.state.stockPrice}</div>
               </section>
             ) : null}
-          </div>
+          </section>
           <section id="buttons">
             <div>
               <button className="entryButton" id="calculate" onClick={this.calculateSalary}>Calculate</button>
